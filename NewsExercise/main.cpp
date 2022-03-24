ae47d260555178cfdd61d677267b4e542d100185
Thanatoslayer6 <thanatoslayer6@gmail.com>
2022-03-24 19:35:28 +0800

    Still a work in progress wait

--- NewsExercise/main.cpp
+++ NewsExercise/main.cpp
@@ -4,23 +4,18 @@
 
 using namespace std;
 
-string database[2][2] = {
+string database[][2] = {
     {"admin12345", "wisdom123"},
-    {"ellejohn", "pass123"}
+    {"ellejohn", "pass123"},
+    {"test", "pp"}
 };
 string username, password;
 int pageNum;
-string choices[5][2] = { 
-    {'f', "Front Page" },
-    {'h', "Headline" },
-    {'e', "Entertainment" },
-    {'s', "Sports Page" },
-    {'d', "Editorial" }
-};
-char option;
+string Pages[5] = { "Front Page", "Headline", "Entertainment", "Sports Page", "Editorial" };
+char option, keyCodes[5] = {'f', 'h', 'e', 's', 'd'};
 
 bool validateCredentials() {
-    for (int i = 0; i < 2; i++){
+    for (int i = 0; i < 3; i++){
         if (database[i][0] == username && database[i][1] == password) {
             return true; // Means username and pwd are correct
         }
@@ -28,10 +23,10 @@ bool validateCredentials() {
     return false;
 }
 
-int checkPage(char choices[]){
+int checkPage(){
     for (int i = 0; i < 5; i++) {
-        if (choices[i] == option || choices[i] == option + 32) { // can be upper or lowercase
-           return i; // Returns the right index
+        if (keyCodes[i] == option || keyCodes[i] == option + 32) { // can be upper or lowercase
+           return i; // Returns the right index for the page
         }
     }
     return -1; // -1 for error
@@ -39,7 +34,7 @@ int checkPage(char choices[]){
 
 void getDate() { // Taken from https://stackoverflow.com/questions/40100507/how-do-i-get-the-current-date-in-c
     std::time_t now_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    cout << "    Date:" << std::ctime(&now_time);
+    cout << "    Date: " << std::ctime(&now_time);
 }
 
 void Login() {
@@ -56,16 +51,21 @@ void Login() {
 
 void Home() {
     // Greet the user
+    cout << R"(     _   ____________  _   __   _   _________       _______    _____ ________________)" << endl;
+    cout << R"(    / | / / ____/ __ \/ | / /  / | / / ____/ |     / / ___/   / ___//  _/_  __/ ____/)" << endl;
+    cout << R"(   /  |/ / __/ / / / /  |/ /  /  |/ / __/  | | /| / /\__ \    \__ \ / /  / / / __/)" << endl;
+    cout << R"(  / /|  / /___/ /_/ / /|  /  / /|  / /___  | |/ |/ /___/ /   ___/ // /  / / / /___)" << endl;
+    cout << R"( /_/ |_/_____/\____/_/ |_/  /_/ |_/_____/  |__/|__//____/   /____/___/ /_/ /_____/)" << endl;
     cout << "\n      ~ [ WELCOME TO THE NEON NEWS SITE (Logged in as " << username << ") ] ~\n";
     cout << "\n     Where do you want to go? ------ Please enter only one character \n\n";
-    cout << "\t* " << choices[0][1] << "     - F\n";
-    cout << "\t* Headline       - H\n";
-    cout << "\t* Entertainment  - E\n";
-    cout << "\t* Sports Page    - S\n";
-    cout << "\t* Editorial      - D\n\n";
-    cout << "\tGO TO ~~~~~~> ";
+
+    // Show possible pages
+    for (int i = 0; i < 5; i++) {
+        cout << "\t* " << Pages[i] << "\t" << "   -   " << keyCodes[i] << "\n";
+    }
+    cout << "\n\tGO TO ~~~~~~> ";
     cin >> option;
-    pageNum = checkPage(choices); // Assign return value of function
+    pageNum = checkPage(); // Assign return value of function
     if (pageNum == -1) {
         throw 8;
     }
@@ -76,6 +76,12 @@ void ShowPage() {
         case 0: // Front page
             cout << "\n.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-FRONT PAGE-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n\n";
             getDate(); // Show date
+             
+            cout << "\n\t ~~~ PCS Students Cramming Over The Upcoming Exams! ~~~\n\n";
+            cout << 
+            "\t As the exams are approaching near, and the school year \n"
+            "\t closing in. Many students have been worried about taking \n"
+            "\t their last exam... [READ MORE AT PAGE ]\n";
             cout << "\n.-=-=-=-=-=-=-=-=-=-=-=-=-END OF FRONT PAGE-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n";
             break;
         case 1: // Headline
